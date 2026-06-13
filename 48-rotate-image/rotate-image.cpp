509aa138579e1d8723478c051b4009049b14cf3d
Shiksha Pandey <shikshapandey2004@gmail.com>
2026-06-13 18:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (34.11%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n =matrix.size();
-
-        //transpose
+        int n=matrix.size();
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
+            
         }
-        //reverse
         for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
+                reverse(matrix[i].begin(),matrix[i].end());            }
     }
 };
